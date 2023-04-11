46314b7d09bdbf741cced986701903995cfaad03
Yaroslav <yaroslav.11.tk@gmail.com>
2023-04-11 12:59:42 +0300

    session

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?
 
-$name = "Petro";
+/*$name = "Petro";
+echo "Hello <br>";
+print $name;*/
 
-echo "Hello";
+session_start();
 
-print $name;
+/*echo $_SESSION['MyName'] . '<br>';
+var_dump($_SESSION['mySettings']);
+echo '<br>';*/
+
+if (!isset($_SESSION['access']))
+    die('You are not an Admin');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+
+    <?php
+    echo "You are an admin";
+        ?>
+        
+</body>
+
+</html>
--- session.php
+++ session.php
@@ -1,3 +1,43 @@
-<?php 
+<?php
+session_start();
 
-?>
+/*$_SESSION['MyName'] = "Valiok";
+$_SESSION['mySettings'] = ['test', 1234, true];
+var_dump($_SESSION);
+echo '<br>';
+unset($_SESSION['MyName']);
+var_dump($_SESSION);*/
+
+// session_unset();
+// session_destroy();
+
+// var_dump($_SESSION);
+if(isset($_GET['logout']) && $_GET['logout'] == 1) {
+    session_unset();
+    header("Location: session.php");
+}; 
+
+define("ADMINPASS", "12345");
+$_SESSION['errors'] = "";
+
+if (!empty($_POST['password_access'])) {
+    if ($_POST['password_access'] === ADMINPASS) {
+        $_SESSION['access'] = true;
+        $_SESSION['errors'] = "Login success";
+    } else {
+        $_SESSION['errors'] = "WRONG PASSWORD!!!";
+    }
+}
+
+ if (isset($_SESSION['access'])) {
+    echo $_SESSION['errors'];
+    echo ' <a href="session.php?logout=1 "><button>Logout</button></a>';
+} else {
+    echo $_SESSION['errors'];
+    ?>
+
+        <form action="" method="post">
+            <input type="text" name="password_access" placeholder="Password">
+            <button>Login</button>
+        </form>
+<?php } ?>
